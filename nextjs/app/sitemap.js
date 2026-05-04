b64759c0cc022b9dabeab5ae26b9c57482d13dea
Avery Findley <227237851+AveryFindley@users.noreply.github.com>
2026-05-04 11:52:01 -0700

    Create sitemap.js

---
+++ nextjs/app/sitemap.js
@@ -0,0 +1,18 @@
+export default function sitemap() {
+    return [
+        { url: 'https://portfolio-nextjs-pink-three.vercel.app/', 
+            lastModified: new Date(),
+        },
+        { url: 'https://portfolio-nextjs-pink-three.vercel.app/about', 
+            lastModified: new Date(),
+        },
+        { url: 'https://portfolio-nextjs-pink-three.vercel.app/projects', 
+            lastModified: new Date(),
+        },
+        
+    ]
+}
+
+//Sitemap is important for SEO because it helps aid the search engine
+//know which pages are most important and when they are updated.
+//This can be helpful for frequently updated pages, and optimizes search engine crawling.
